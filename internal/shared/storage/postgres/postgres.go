d4234f694ed43f87a30d1dc66d493ba932460436
Arlandaren <bad-07@list.ru>
2024-11-11 02:07:15 +0900

    chore: postgres conn

---
+++ internal/shared/storage/postgres/postgres.go
@@ -0,0 +1,25 @@
+package postgres
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"service/internal/shared/config"
+	"service/internal/shared/storage/dto"
+)
+
+func InitDB() (*gorm.DB, error) {
+	dsn, err := config.GetPostgres()
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := gorm.Open(postgres.Open(dsn.ConnStr), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	err = db.AutoMigrate(&dto.User{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
