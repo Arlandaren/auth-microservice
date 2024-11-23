41a6e9f30d763b3e7db0b87d7a3f38e345cfe3a1
Arlandaren <bad-07@list.ru>
2024-11-23 18:46:59 +0900

    chore: logs

--- auth-backend/internal/transport/http/handlers.go
+++ auth-backend/internal/transport/http/handlers.go
@@ -2,11 +2,10 @@ package httpserver
 
 import (
 	"encoding/json"
-	"fmt"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 	"net/url"
 	"service/internal/service"
-
 	pb "service/pkg/grpc/auth_v1"
 )
 
@@ -21,64 +20,87 @@ func NewServer(service *service.Service) *Server {
 }
 
 func (s *Server) LoginHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Получен запрос на логин от %s", r.RemoteAddr)
+
 	redirectURI := r.URL.Query().Get("redirect_uri")
 	if redirectURI == "" {
 		redirectURI = "http://localhost:3001"
 	}
-
-	fmt.Println("asdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasd")
+	log.Printf("Redirect URI: %s", redirectURI)
 
 	var req pb.LoginRequest
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&req); err != nil {
+		log.Printf("Ошибка при декодировании запроса логина: %v", err)
 		http.Error(w, "Invalid request", http.StatusBadRequest)
 		return
 	}
 
+	log.Printf("Попытка логина пользователя: %s", req.Name)
+
 	resp, err := s.Service.Login(r.Context(), &req)
 	if err != nil {
+		log.Printf("Ошибка логина для пользователя %s: %v", req.Name, err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	// Создаем URL для перенаправления с токеном в фрагменте URL
+	log.Printf("Успешный логин пользователя: %s", req.Name)
+
 	redirectURL, err := url.Parse(redirectURI)
 	if err != nil {
+		log.Printf("Неверный redirect URI: %s, ошибка: %v", redirectURI, err)
 		http.Error(w, "Invalid redirect URI", http.StatusBadRequest)
 		return
 	}
 
 	redirectURL.Fragment = url.Values{"token": {resp.Token}}.Encode()
+
+	log.Printf("Перенаправление пользователя %s на URL: %s", req.Name, redirectURL.String())
 
 	http.Redirect(w, r, redirectURL.String(), http.StatusFound)
 }
 
+// Обработчик для регистрации
 func (s *Server) RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Получен запрос на регистрацию от %s", r.RemoteAddr)
+
 	redirectURI := r.URL.Query().Get("redirect_uri")
 	if redirectURI == "" {
-		redirectURI = "http://localhost:3001" // Адрес вашего Posty приложения
+		redirectURI = "http://localhost:3001"
 	}
+	log.Printf("Redirect URI: %s", redirectURI)
 
 	var req pb.RegisterRequest
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&req); err != nil {
+		log.Printf("Ошибка при декодировании запроса регистрации: %v", err)
 		http.Error(w, "Invalid request", http.StatusBadRequest)
 		return
 	}
 
+	// Не рекомендуется логировать чувствительную информацию, такую как пароли
+	log.Printf("Попытка регистрации пользователя: %s", req.Name)
+
 	resp, err := s.Service.Register(r.Context(), &req)
 	if err != nil {
+		log.Printf("Ошибка регистрации для пользователя %s: %v", req.Name, err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	log.Printf("Успешная регистрация пользователя: %s", req.Name)
+
 	redirectURL, err := url.Parse(redirectURI)
 	if err != nil {
+		log.Printf("Неверный redirect URI: %s, ошибка: %v", redirectURI, err)
 		http.Error(w, "Invalid redirect URI", http.StatusBadRequest)
 		return
 	}
 
 	redirectURL.Fragment = url.Values{"token": {resp.Token}}.Encode()
+
+	log.Printf("Перенаправление пользователя %s на URL: %s", req.Name, redirectURL.String())
 
 	http.Redirect(w, r, redirectURL.String(), http.StatusFound)
 }
